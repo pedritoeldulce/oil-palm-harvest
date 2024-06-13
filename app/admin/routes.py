3d2de92e88e5e0ca61163179f218e917b7fd02cc
pedritoeldulce <franceskolyperez@gmail.com>
2024-06-13 12:31:20 -0500

    centrado de botones y formularios 2

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -81,7 +81,7 @@ def parcelas():
     nombres = Encargado.name_encargado(a)  # lista de nombres
 
     par_nom = list(zip(list_parcelas, nombres))
-    return render_template('parcela.html', title="Parcelas", parcela_encargado=par_nom)
+    return render_template('parcela/parcela.html', title="Parcelas", parcela_encargado=par_nom)
 
 
 @admin.route('/parcela/edit/<int:parcela_id>', methods=['GET', 'POST'])
@@ -101,7 +101,7 @@ def parcela_edit(parcela_id):
         if parcela_updated:
             flash("Parcela actualidad Exitosamente", "success")
             return redirect(url_for('admin.parcelas'))
-    return render_template('parcela_edit.html', title="Editar Parcela", form=form)
+    return render_template('parcela/parcela_edit.html', title="Editar Parcela", form=form)
 
 
 @admin.route('/parcela/delete/<int:parcela_id>')
@@ -128,7 +128,7 @@ def parcela_form():
             flash("Parcela Registrada Exitosamente", "success")
         return redirect(url_for('admin.parcelas'))
 
-    return render_template('parcela_form.html', title="Parcelas", parcelas=parcelas, form=form)
+    return render_template('parcela/parcela_form.html', title="Parcelas", parcelas=parcelas, form=form)
 
 
 @admin.route('/dashboard/cosecha/new', methods=['POST', 'GET'])
@@ -145,7 +145,7 @@ def cosecha_new():
             flash("Cosecha guardado exitosamente", "success")
         return redirect(url_for('admin.cosecha'))
 
-    return render_template('cosecha_form.html', title="Cosechas", cosechas=cosechas)
+    return render_template('cosecha/cosecha_form.html', title="Cosechas", cosechas=cosechas)
 
 
 @admin.route('/cosecha/edit/<int:cosecha_id>', methods=['GET', 'POST'])
@@ -162,7 +162,7 @@ def cosecha_edit(cosecha_id):
             flash("Cosecha Actualizada exitosamente", "success")
             return redirect(url_for('admin.cosecha'))
 
-    return render_template('cosecha_edit.html', title="Editar Cosecha", form=form)
+    return render_template('cosecha/cosecha_edit.html', title="Editar Cosecha", form=form)
 
 
 @admin.route('/cosecha/delete/<int:cosecha_id>')
@@ -189,7 +189,7 @@ def cosecha():
     # encargados = Encargado().get_encargados()
     #
     # parcela_encargado = list(zip(l_parcelas, encargados))
-    return render_template('cosecha.html', title="Lista de Cosechas", cosecha_parcela=cosecha_parcela)
+    return render_template('cosecha/cosecha.html', title="Lista de Cosechas", cosecha_parcela=cosecha_parcela)
 
 
 @admin.route('/dashboard/puesto-form/<int:id>', methods=['GET', 'POST'])
@@ -204,60 +204,6 @@ def puesto_form(id:None):
     return render_template('puesto_form.html', title="Puesto", form=form)
 
 
-""" @admin.route('/dashboard/cosechas-jcm', methods=['GET', 'POST'])
-def cosechasjcm():
-    jcm = cosechaJCM(request.form)
-
-    encargado,n_cosecha, n_bolsas = jcm.encargado.data, jcm.n_cosecha.data, jcm.bolsas.data
-    p1, p2, p3, p4, p5, p6, p7, p8 = jcm.puesto_1.data, jcm.puesto_2.data, jcm.puesto_3.data, jcm.puesto_4.data, jcm.puesto_5.data, jcm.puesto_6.data, jcm.puesto_7.data, jcm.puesto_8.data
-
-    total = p1+p2+p3+p4+p5+p6+p7+p8
-
-    if jcm.validate_on_submit():
-        # utilizado para guardar los valores en la bd, ya pasaron validate y submit
-        # se procede a guardar los datos capturados
-        cosecha_jcm = CosechaJCM(encargado = encargado, n_cosecha = n_cosecha, n_bolsas=n_bolsas, p_1 = p1, 
-                                 p_2=p2, p_3=p3, p_4=p4, p_5=p5, p_6=p6, p_7=p7, p_8=p8, total = total )
-
-        cosecha_jcm.save()
-
-    return render_template('cosechas_jcm.html', title = "Cosechas - JCM", jcm = jcm, total = total)
-
-@admin.route('/dashboard/cosechas-bat', methods=['GET', 'POST'])
-def cosechasbat():
-    bat = cosechaBAT(request.form)
-
-    encargado,n_cosecha, n_bolsas = bat.encargado.data, bat.n_cosecha.data, bat.bolsas.data
-    p1, p2, p3, p4, p5, p6, p7, p8 = bat.puesto_1.data, bat.puesto_2.data, bat.puesto_3.data, bat.puesto_4.data, bat.puesto_5.data, bat.puesto_6.data, bat.puesto_7.data, bat.puesto_8.data
-    p9, p10, p11, p12 = bat.puesto_9.data, bat.puesto_10.data, bat.puesto_11.data, bat.puesto_12.data
-
-    total = p1+p2+p3+p4+p5+p6+p7+p8+p9+p10+p11+p12
-
-    if bat.validate_on_submit():
-        cosecha_bat = CosechaBAT(encargado=encargado, n_cosecha = n_cosecha, n_bolsas=n_bolsas, p_1=p1, p_2=p2, p_3=p3, p_4=p4, p_5=p5,
-                                 p_6=p6, p_7=p7, p_8=p8, p_9=p9, p_10=p10, p_11=p11, p_12=p12, total =total)
-
-        cosecha_bat.save()
-        
-    return render_template('cosechas_bat.html', title = "Cosechas - Buenos Aires", bat = bat, total = total)
-
-@admin.route('/dashboard/cosechas-km6')
-def cosechaskm6():
-    km6 = cosechaKM6()
-    return render_template('cosechas_km6.html', title = "Cosechas - Buenos Aires", km6 = km6) """
-
-
-""" @admin.route('/dashboard/empleados', methods=['GET','POST'])
-def empleados():
-    form = EmpleadosForm(request.form)
-    nombres, apellidos,dni, telefono=form.nombres.data, form.apellidos.data, form.dni.data, form.telefono.data
-    
-    if form.validate_on_submit():
-
-        empleado = Empleados(nombres = nombres, apellidos=apellidos, dni = dni, telefono = telefono)
-        empleado.save()
-    return render_template('empleados.html', title = "Empleado", form=form)
- """
 
 
 @admin.app_errorhandler(404)
